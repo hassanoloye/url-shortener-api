2369145bf3dae0bd2ac2981920f61361a06f5b6b
Hassan Oyeboade <oyeboadehassan@gmail.com>
2023-03-01 19:01:57 +0100

    implement url shortener

--- validators/schema.py
+++ validators/schema.py
@@ -1,5 +1,18 @@
-from pydantic import BaseModel, HttpUrl
+from pydantic import BaseModel, HttpUrl, validator, ValidationError
+
+
+class URL(BaseModel):
+    value: HttpUrl
 
 
 class ShortenRequest(BaseModel):
-    url: HttpUrl
+    url: str
+
+    @validator('url')
+    def valid_url(cls, value):
+        try:
+            parsed_value = value if value.startswith('http://') or value.startswith('https://') else f'http://{value}'
+            URL(value=parsed_value)
+            return value
+        except ValidationError as e:
+            raise ValueError('Not a valid url')
